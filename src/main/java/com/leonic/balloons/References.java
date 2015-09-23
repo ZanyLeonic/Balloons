76f77cdbf0cf85b67df4f8c2a73e91a037a58da0
ZanyLeonic <leo.durrant@outlook.com>
2015-09-23 22:36:24 +0100

    Finalizing for Version 0.2

--- src/main/java/com/leonic/balloons/References.java
+++ src/main/java/com/leonic/balloons/References.java
@@ -2,7 +2,7 @@ package com.leonic.balloons;
 /**
  * A class that holds most of the most used strings in this mod.
  * @author ZanyLeonic
- * @version 0.1
+ * @version 0.2
  * 
  */
 public class References {
@@ -19,7 +19,7 @@ public class References {
     /**
      * The version of the Mod.
      */
-    public static final String VERSION = "1.7.10-0.1";
+    public static final String VERSION = "1.7.10-0.2";
     
     /**
      * The location of the CommonProxy.
